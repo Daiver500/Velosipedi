dca40a1f9282b1290c7d0acce825b5dbcc1cbac7
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2021-01-20 16:53:10 +0300

    CSS start

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,13 +3,13 @@ const navigationToggleOpen = document.querySelector(".navigation__toggle-open")
 const navigationMenu = document.querySelector(".navigation__destinations")
 
 const navigationClose = () => {
-  navigationMenu.classList.add("navigation__hidden");
+  navigationMenu.classList.add("hidden");
   navigationToggleClose.classList.add("hidden");
   navigationToggleOpen.classList.remove("hidden");
 }
 
 const navigationOpen = () => {
-  navigationMenu.classList.remove("navigation__hidden");
+  navigationMenu.classList.remove("hidden");
   navigationToggleClose.classList.remove("hidden");
   navigationToggleOpen.classList.add("hidden");
 }
